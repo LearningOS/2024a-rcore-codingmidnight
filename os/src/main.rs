0b517ad3502c8fd659a94620fbbe489a483afa1f
debian@utm <debian@localhost>
2024-11-04 09:09:12 -0800

    完成复现第一章实验内容

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,12 +1,17 @@
 #![no_std]
 #![no_main]
-
+#![feature(panic_info_message)]
 // use core::fmt;
 // use core::fmt::Write;
+#[macro_use]
+mod console;
 mod lang_items;
+mod logging;
+use log::*;
 core::arch::global_asm!(include_str!("entry.asm"));
 mod sbi;
 use sbi::shutdown;
+
 fn clear_bss(){
     extern "C"{
         fn sbss();
@@ -18,7 +23,39 @@ fn clear_bss(){
 }
 #[no_mangle]
 pub fn rust_main()->!{
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
     clear_bss();
+    logging::init();
+    println!("[kernel] Hello, world!");
+    trace!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    warn!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack as usize
+    );
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     shutdown();
 }
 // // println!("Hello, world!");
